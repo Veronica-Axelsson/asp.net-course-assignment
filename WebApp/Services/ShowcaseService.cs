95a1e59788b7970390a9cdd13e63799b5c70160f
Veronica Axelsson <millernadragon@hotmail.com>
2023-05-27 22:53:49 +0200

    CSS and cleaned some code

--- WebApp/Services/ShowcaseService.cs
+++ WebApp/Services/ShowcaseService.cs
@@ -17,17 +17,17 @@ public class ShowcaseService
             Url = "/products",
         }
      },
-    new ShowcaseModel()
-    {
-        Ingress = "BMERKETO THE BEST A PERSON CAN GET",
-        Title = "Now with all new spices",
-        ImageUrl = "images/placeholders/625x647.svg",
-        Button = new LinkButtonModel
-        {
-            Content = "SHOP NOW",
-            Url = "/products",
-        }
-     }
+    //new ShowcaseModel()
+    //{
+    //    Ingress = "BMERKETO THE BEST A PERSON CAN GET",
+    //    Title = "Now with all new spices",
+    //    ImageUrl = "images/placeholders/625x647.svg",
+    //    Button = new LinkButtonModel
+    //    {
+    //        Content = "SHOP NOW",
+    //        Url = "/products",
+    //    }
+    // }
     };
 
 
